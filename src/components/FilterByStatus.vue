e459a95906361df7cf32e348bf95ac6139b751f1
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-17 16:14:35 -0300

    filter structure

---
+++ src/components/FilterByStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="filter">
+    <div class="item">
+        All
+    </div>
+    <div class="item">
+        Alive
+    </div>
+    <div class="item">
+        Dead
+    </div>
+    <div class="item">
+        Unknown    
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
