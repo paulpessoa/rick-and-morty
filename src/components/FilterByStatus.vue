f69b4fc51302abcb9392cb4eb5f957a1af612549
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-17 16:29:01 -0300

    style filters

--- src/components/FilterByStatus.vue
+++ src/components/FilterByStatus.vue
@@ -21,6 +21,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+    .filter {
+        width: 400px;
+        margin: 0 auto;
+        display:grid;
+        grid-template-columns: repeat(4, 1fr);
+        border-radius: 10px;
+        overflow: hidden;
+        .item {
+            padding: 1rem 0.5rem;
+            background-color: var(--background-card);
+            text-align: center;
+            cursor: pointer;
+            &:hover{
+                color: var(--text-orange);
+            }
+        }
+    }
 </style>
