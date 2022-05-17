6e7d64e050989a1cfe8a22359812997efaf5b64a
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-17 17:23:22 -0300

    filter by name function

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
   <h1>Rick and Morty</h1>
     <FilterByStatus />
+    <FilterByName />
     <ListCharacters />
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script>
 import ListCharacters from './components/ListCharacters.vue';
 import FilterByStatus from './components/FilterByStatus.vue';
+import FilterByName from './components/FilterByName.vue';
 
 export default {
   name: 'App',
   components: {
     ListCharacters,
-    FilterByStatus
+    FilterByStatus,
+    FilterByName
 }
 }
 </script>
